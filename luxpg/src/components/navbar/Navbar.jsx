395f2b071ee5a74993a84776182e4e1aad7a5c55
Ashton607 <portmanashton4@gmail.com>
2026-06-04 18:13:45 +0200

    added navbar

---
+++ luxpg/src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Landscape</li>
+            <li>Weddings</li>
+            <li>Potraits</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
