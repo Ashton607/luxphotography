93c610bcfacc115f245980bcb9a38abf0d26d4ff
Ashton607 <portmanashton4@gmail.com>
2026-06-08 13:56:58 +0200

    finished ceremony section

--- luxpg/src/components/weddings/Weddings.jsx
+++ luxpg/src/components/weddings/Weddings.jsx
@@ -4,6 +4,7 @@ import './Weddings.css'
 const Weddings = () => {
   return (
     <div className="weddings">
+      {/*Using the same classNames from landscape.css allows for consistent styling since the css is global*/}
 
       {/*Ceremony*/}
       <section className="weddings-section">
@@ -13,21 +14,21 @@ const Weddings = () => {
        <div className="card ceremony-card-1">
         <div className="card-overlay">
           <span className="card-name">James Lux</span>
-          <p className="card-desc">City Lights, Timeless Love</p>
+          <p className="card-desc">An elegant promise, sliding forever onto her finger.</p>
         </div>
        </div>
 
        <div className="card ceremony-card-2">
         <div className="card-overlay">
           <span className="card-name">James Lux</span>
-          <p className="card-desc">City Lights, Timeless Love</p>
+          <p className="card-desc">Whispered vows captured in a single, breathless gaze.</p>
         </div>
        </div>
 
        <div className="card ceremony-card-3">
         <div className="card-overlay">
           <span className="card-name">James Lux</span>
-          <p className="card-desc">City Lights, Timeless Love</p>
+          <p className="card-desc">Pure celebration, capturing their shared joy on our lens.</p>
         </div>
        </div>
       </div>
